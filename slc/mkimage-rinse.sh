1bfe7527fda3a42a14446112616e233cc6c786e3
Sebastien Binet <binet@cern.ch>
2013-12-05 16:52:27 +0100

    rinse: add bashrc and bash_profile

--- slc/mkimage-rinse.sh
+++ slc/mkimage-rinse.sh
@@ -85,6 +85,32 @@ sudo rm -rf sbin/sln
 sudo rm -rf etc/ld.so.cache var/cache/ldconfig
 sudo mkdir -p --mode=0755 var/cache/ldconfig
 
+## create a nice login bash shell
+sudo cat >| /root/.bashrc <<EOF
+# Personal aliases and functions.
+
+# Personal environment variables and startup programs should go in
+# ~/.bash_profile.  System wide environment variables and startup
+# programs are in /etc/profile.  System wide aliases and functions are
+# in /etc/bashrc.
+
+if [ -f "/etc/bashrc" ] ; then
+  source /etc/bashrc
+fi
+EOF
+
+cat > /root/.bash_profile << EOF
+# Personal environment variables and startup programs.
+
+# Personal aliases and functions should go in ~/.bashrc.  System wide
+# environment variables and startup programs are in /etc/profile.
+# System wide aliases and functions are in /etc/bashrc.
+
+if [ -f "$HOME/.bashrc" ] ; then
+  source $HOME/.bashrc
+fi
+EOF
+
 # to restore locales later:
 #  yum reinstall glibc-common
 
